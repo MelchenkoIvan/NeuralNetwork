caf6691390fdf981af71c36e7d5041e5a2822092
imelchenko61@gmail.com <imelchenko61@gmail.com>
2022-08-17 22:33:14 +0200

    Added NN and NNT

---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var a = new List<string>();
+a.Add("a");
+a.Add("b");
+a.Add("c");
+var b = new List<string>();
+b.Add("a");
+b.Add("b");
+b.Add("d");
+
+UsunZdublowane(a, b);
+
+
+void UsunZdublowane(List<string> l1, List<string> l2)
+{
+    var a = l1.RemoveAll(x => l2.Contains(x));
+
+}
---
+++ NeuralNetworkTests1/NeuralNetworkTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NeuralNetwork.Tests
+{
+    [TestClass()]
+    public class NeuralNetworkTests
+    {
+        [TestMethod()]
+        public void FeedForwardTest()
+        {
+            var outputs = new double[] { 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 };
+            var inputs = new double[,]
+            {
+                // Результат - Пациент болен - 1
+                //             Пациент Здоров - 0
+
+                // Неправильная температура T
+                // Хороший возраст A
+                // Курит S
+                // Правильно питается F
+                //T  A  S  F
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 1, 1 },
+                { 0, 1, 0, 0 },
+                { 0, 1, 0, 1 },
+                { 0, 1, 1, 0 },
+                { 0, 1, 1, 1 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 1, 0 },
+                { 1, 0, 1, 1 },
+                { 1, 1, 0, 0 },
+                { 1, 1, 0, 1 },
+                { 1, 1, 1, 0 },
+                { 1, 1, 1, 1 }
+            };
+
+            var topology = new Topology(4, 1, 0.1, 2);
+            var neuralNetwork = new NeuralNetwork(topology);
+            var difference = neuralNetwork.Learn(outputs, inputs, 10000);
+
+            var results = new List<double>();
+            for (int i = 0; i < outputs.Length; i++)
+            {
+                var row = NeuralNetwork.GetRow(inputs, i);
+                var res = neuralNetwork.Predict(row).Output;
+                results.Add(res);
+            }
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                var expected = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+        [TestMethod()]
+        public void DataSetTest()
+        {
+            var outputs = new List<double>();
+            var inputs = new List<double[]>();
+            using (var sr = new StreamReader("heart.csv"))
+            {
+                var header = sr.ReadLine();
+
+                while (!sr.EndOfStream)
+                {
+                    var row = sr.ReadLine();
+                    var values = row.Split(',').Select(v => Convert.ToDouble(v)).ToList();
+                    var output = values.Last();
+                    var input = values.Take(values.Count - 1).ToArray();
+
+                    outputs.Add(output);
+                    inputs.Add(input);
+                }
+            }
+            var inputSignals = new double[inputs.Count,inputs[0].Length];
+            var a = inputSignals.GetLength(0);
+            var b = inputSignals.GetLength(1);
+
+            for (int i = 0; i < inputSignals.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputSignals.GetLength(1); j++)
+                {
+                    inputSignals[i, j] = inputs[i][j];
+                }
+            }
+            var topology = new Topology(outputs.Count, 1, 0.1, outputs.Count / 2);
+            var neuralNetwork = new NeuralNetwork(topology);
+            var difference = neuralNetwork.Learn(outputs.ToArray(), inputSignals, 10000);
+
+            var results = new List<double>();
+            for (int i = 0; i < outputs.Count; i++)
+            {
+                var row = inputs[i];
+                var res = neuralNetwork.Predict(inputs[i]).Output;
+                results.Add(res);
+            }
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                var expected = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
